6696b77cdb23f447584761407b8248b38524aa8c
vs.kavin <kavinvs90@gmail.com>
2025-03-18 09:51:11 +0530

    day

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import DestinationContainer from "./components/DestinationContainer";
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      {/* Your code here */}
+    <div className="App">
+      <DestinationContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
